0900270a6f5b78ea9bb0f7e0cfbbbcc6f1d56b69
Nityasaha13 <nityasaha39@gmail.com>
2024-06-03 02:08:08 +0530

    show thumbnail single post page

--- resources/views/Blog/single.blade.php
+++ resources/views/Blog/single.blade.php
@@ -4,6 +4,10 @@
 
 <div class="row">
   <div class="col-md-12">  <div class="card h-100 w-100 m-2">  <div class="card-body">
+    <p>
+      @if($post->thumbnail != '')
+        <img src="/storage/{{ $post->thumbnail }}" style="width:100%; height:400px; object-fit:cover; object-position: center center;"></p>
+      @endif
         <h3 class="card-title">{{$post->title}}</h3>
         <p class="card-text mt-1" style="font-size:14px; color:rgb(0, 68, 255)">
           @foreach ($post->categories as $category)
